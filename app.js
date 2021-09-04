2a28c2c4caf7e5ffa7600f3ea0bf22749140216f
Zakaria Outahar <outaharz@gmail.com>
2021-09-04 10:35:39 +0200

    JavaScript : affichage des cookies, suppression au clic et correction des derniers bugs rencontrés

--- app.js
+++ app.js
@@ -30,12 +30,19 @@ function btnAction(e){
     if(description === "generer"){
         genererCookie(nvObj.cookieName, nvObj.cookieValue, nvObj.cookieExpire);
     } else if(description === "toutAfficher"){
-        listeCookies();
+        if(affichage.childElementCount > 0){
+            infoTxt.innerText = "Erreur : l'affichage est déjà en cours, cliquez sur un cookie pour le supprimer.";
+        } else{
+            listeCookies();
+        }
     }
 }
 
+// Génération des cookies
+
 function genererCookie(name, value, exp){
     infoTxt.innerText = "";
+    affichage.innerHTML = "";
 
     let cookies = document.cookie.split(';')
     cookies.forEach(cookie => {
@@ -62,5 +69,36 @@ function genererCookie(name, value, exp){
     let info = document.createElement('li');
     info.innerText = `Cookie ${name} généré.`;
     affichage.appendChild(info);
-    setTimeout(() => info.remove(), 1500)
+    setTimeout(() => info.remove(), 1500);
+}
+
+// Affichage des cookies
+
+function listeCookies(){
+    let cookies = document.cookie.split(';')
+    if(cookies.join() === ""){
+        infoTxt.innerText = "Pas de cookies à afficher";
+        return;
+    }
+
+    cookies.forEach(cookie => {
+        cookie = cookie.trim();
+        let formatCookie = cookie.split('=');
+        
+        let item = document.createElement('li');
+        infoTxt.innerText = "Cliquez sur un cookie pour le supprimer."
+
+        item.innerText = `Nom : ${decodeURIComponent(formatCookie[0])}, Valeur : ${decodeURIComponent(formatCookie[1])}`;
+        affichage.appendChild(item);
+
+        // Suppression cookie
+
+        item.addEventListener('click', () => {
+            document.cookie = `${formatCookie[0]}=; expires=${new Date(0)}`
+            item.innerText = `Cookie ${formatCookie[0]} supprimé`;
+            setTimeout(() => {
+                item.remove();
+            }, 1000);
+        })
+    })
 }
